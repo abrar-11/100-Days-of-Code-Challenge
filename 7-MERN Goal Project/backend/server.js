3259b73f494b66dcafa22e550c3c3f1b67ec9059
Abrar Ahmed <abrarahmed2185@gmail.com>
2022-07-19 23:47:17 +0500

    Deploying

--- 7-MERN Goal Project/backend/server.js
+++ 7-MERN Goal Project/backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path  =require("path")
 const dotenv = require("dotenv").config();
 const app = express();
 const { errorHandler } = require("./middleware/error");
 const colors = require("colors");
 const connectDB = require("./config/database");
+
 
 // ======Importing Routes======
 const goalRoutes = require("./routes/goalRoutes");
@@ -25,6 +27,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/goals", goalRoutes);
 
 app.use("/api/user", userRoutes);
+
+if(process.env.NODE_ENVIRONMENT == 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname, '../','frontend','build','index.html'))
+    })
+}else{
+    app.get('/',(req,res)=>res.send("PLease Set to Production Mode.."))
+}
+
+
 
 
 // Error MiddleWare
