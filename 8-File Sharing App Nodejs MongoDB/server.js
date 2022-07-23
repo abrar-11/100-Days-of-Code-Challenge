1d686b8fbe7c1ef019caa83b27442db2261861a4
Abrar Ahmed <abrarahmed2185@gmail.com>
2022-07-23 22:04:00 +0500

    Fixing minor Bug

--- 8-File Sharing App Nodejs MongoDB/server.js
+++ 8-File Sharing App Nodejs MongoDB/server.js
@@ -10,6 +10,7 @@ const port = process.env.port || 5000;
 const upload = multer({ dest: "uploads" });
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs");
 
 // Home Page Route
