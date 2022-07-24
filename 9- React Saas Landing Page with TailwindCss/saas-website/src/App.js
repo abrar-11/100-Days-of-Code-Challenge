a7dd799e0d779e8277f5c6050dcfed02d73b943e
Abrar Ahmed <abrarahmed2185@gmail.com>
2022-07-24 13:42:40 +0500

    Header

--- 9- React Saas Landing Page with TailwindCss/saas-website/src/App.js
+++ 9- React Saas Landing Page with TailwindCss/saas-website/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Hero } from "./components";
+import Header from "./layouts/Header";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+   return (
+      <>
+         <div className="container mx-auto w-10/12 max-w-6xl">
 
-export default App
+          {/*=========  Header Section========= */}
+            <Header/>
+
+            {/*=========  Hero Section========= */}
+            <Hero/>
+         </div>
+      </>
+   );
+};
+
+export default App;
