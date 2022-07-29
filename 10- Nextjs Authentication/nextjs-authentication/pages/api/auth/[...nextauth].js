e2b7e21a323f875375d66922943a9b6e78286b4b
Abrar Ahmed <abrarahmed2185@gmail.com>
2022-07-30 00:05:03 +0500

    MongoDb Adapter Addtion

--- 10- Nextjs Authentication/nextjs-authentication/pages/api/auth/[...nextauth].js
+++ 10- Nextjs Authentication/nextjs-authentication/pages/api/auth/[...nextauth].js
@@ -2,8 +2,13 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "../../../Database/mongodb"
+
+
 export default NextAuth({
   // Configure one or more authentication providers
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
@@ -12,7 +17,8 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
-    })
+    }),
+    
    
     // ...add more providers here
   ],
@@ -20,4 +26,5 @@ export default NextAuth({
     signIn: '/auth/signIn',
   }
 
-})
+},
+)
