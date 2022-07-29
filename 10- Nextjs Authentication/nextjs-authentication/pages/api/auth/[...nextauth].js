390cda449d6638186696fb7b1b9d0e93eb06f002
Abrar Ahmed <abrarahmed2185@gmail.com>
2022-07-30 00:18:09 +0500

    Magic Email Provider  Sign In

--- 10- Nextjs Authentication/nextjs-authentication/pages/api/auth/[...nextauth].js
+++ 10- Nextjs Authentication/nextjs-authentication/pages/api/auth/[...nextauth].js
@@ -1,30 +1,39 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from  "next-auth/providers/email";
 
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
-import clientPromise from "../../../Database/mongodb"
-
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../Database/mongodb";
 
 export default NextAuth({
-  // Configure one or more authentication providers
-  adapter: MongoDBAdapter(clientPromise),
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-    }),
-    
-   
-    // ...add more providers here
-  ],
-  pages:{
-    signIn: '/auth/signIn',
-  }
+   // Configure one or more authentication providers
+   adapter: MongoDBAdapter(clientPromise),
+   providers: [
+      GithubProvider({
+         clientId: process.env.GITHUB_ID,
+         clientSecret: process.env.GITHUB_SECRET,
+      }),
+      GoogleProvider({
+         clientId: process.env.GOOGLE_ID,
+         clientSecret: process.env.GOOGLE_SECRET,
+      }),
 
-},
-)
+      EmailProvider({
+         server: {
+            host: process.env.EMAIL_SERVER_HOST,
+            port: process.env.EMAIL_SERVER_PORT,
+            auth: {
+               user: process.env.EMAIL_SERVER_USER,
+               pass: process.env.EMAIL_SERVER_PASSWORD,
+            },
+         },
+         from: process.env.EMAIL_FROM,
+      }),
+
+      // ...add more providers here
+   ],
+   pages: {
+      signIn: "/auth/signIn",
+   },
+});
