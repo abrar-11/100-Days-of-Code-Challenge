06c6b88b2c51f495ba8c7ed7c53246e799dc1e56
Abrar Ahmed <abrarahmed2185@gmail.com>
2022-07-30 00:44:13 +0500

    Bugs Fixes

--- 10- Nextjs Authentication/nextjs-authentication/pages/auth/signIn.jsx
+++ 10- Nextjs Authentication/nextjs-authentication/pages/auth/signIn.jsx
@@ -1,16 +1,19 @@
 import { useSession, signIn } from "next-auth/react";
 import { useRouter } from "next/router";
-
-
+import { useState } from "react";
 
 const signin = () => {
    const { data: sessions, status } = useSession();
    const { push } = useRouter();
-
+   const [email, setEmail] = useState("");
    const providers = [
       {
          icon: " ",
          name: "github",
+      },
+      {
+         icon: " ",
+         name: "google",
       },
       {
          icon: " ",
@@ -21,7 +24,13 @@ const signin = () => {
    const handleSignIn = (provider) => {
       signIn(provider);
    };
-
+   const handleEmailSubmit = (e) => {
+      e.preventDefault();
+      if (email) {
+        console.log("imin");
+         signIn("email", { email ,redirect:false });
+      }
+   };
 
    if (status === "loading") {
       return <h2>Checking Authentication Please Wait .. </h2>;
@@ -38,6 +47,14 @@ const signin = () => {
 
    return (
       <div>
+         <form action="" onSubmit={handleEmailSubmit}>
+            <input
+               type="email"
+               value={email}
+               onChange={(e) => setEmail(e.target.value)}
+            />
+            <button type="submit">Login</button>
+         </form>
          {providers.map((provider) => {
             return (
                <button onClick={() => handleSignIn(provider.name)}>
